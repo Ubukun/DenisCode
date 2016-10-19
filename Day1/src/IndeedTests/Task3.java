1e8e9990e14be3b98f49fec83a90664a121c64bc
Denis Sobenn <siboor69@hotmail.com>
2016-10-18 23:16:11 -0700

    Committed second time

--- Day1/src/IndeedTests/Task3.java
+++ Day1/src/IndeedTests/Task3.java
@@ -11,10 +11,10 @@ public class Task3 {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\ds\\Selenium\\TrainningFiles\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		String expectedName = "Invoice";
+		String expectedName = "INVOICE";
 		String actualName = "";
-		//String expectedTourName = "Denis Adventure";
-		//String actualTourName = "";
+		String expectedCustName = "Denis Sobenn";
+		String actualCustName = "";
 		
 		// Navigate to "Phptravels" customer website
 		driver.get("http://www.phptravels.net");
@@ -46,39 +46,35 @@ public class Task3 {
 		// Click on the "Book Now" button
 		driver.findElement(By.xpath(".//*[@id='OVERVIEW']/div[2]/div/form[2]/div/div[2]/button")).click();
 		Thread.sleep(2000);
-		// Click on the "Confirm this booking" button
+		// Scroll to the bottom of the page to confirm the booking
 		JavascriptExecutor js = (JavascriptExecutor)driver;
 		js.executeScript("scrollBy(0,500)");
 		Thread.sleep(2000);
+		// Click on the "Confirm this booking" button
 		driver.findElement(By.xpath(".//*[@id='top']/div[7]/div/div/div[1]/div/div[2]/div[5]/button")).click();
 		Thread.sleep(2000);
+		JavascriptExecutor js1 = (JavascriptExecutor)driver;
+		js1.executeScript("scrollBy(0,500)");
+		Thread.sleep(2000);
 		
-		System.out.println (driver.findElement(By.xpath(".col-md-6>div")).getText());
-		// Verify if invoice have been created successfully validation
-		actualName = driver.findElement(By.xpath(".col-md-6>div")).getText();
+		// Verify if invoice have been created successfully
+		actualName = driver.findElement(By.xpath(".//*[@id='top']/div[7]/div/div[7]/div[1]/div[2]")).getText();
 		if (actualName.contentEquals(expectedName)){
-				 System.out.println("Test PASSED! The 'INVOICE' is displayed after booking" );
+				 System.out.println("Test PASSED! The 'INVOICE' label is displayed on invoice after booking" );
 			}else{
-			    System.out.println("Test FAILED! The 'INVOICE' is not displayed");
+			   System.out.println("Test FAILED! The invoice with 'INVOICE' label was not generated");
 			}
-		// Verify if new account have been created successfully with account holder last name validation
-		//actualTourName = driver.findElement(By.xpath(".//*[@id='top']/div[7]/div/div[9]/table/tbody/tr[1]/td[2]/strong[1]")).getText();
-		//if (actualTourName.contentEquals(expectedTourName)){
-		     //System.out.println("Test PASSED! New 'Denis Adventure' tour name is displayed in the Invoice");
-		   //}else{
-			 // System.out.println("Test FAILED! New 'Denis Adventure' tour name was not displayed in the Invoice");
-			//}
+		
+		// Verify the customer full name on the invoice after booking the newly created Tour
+		actualCustName = driver.findElement(By.xpath(".//*[@id='top']/div[7]/div/div[7]/div[2]/p")).getText();
+		if (actualCustName.contentEquals(expectedCustName)){
+		     System.out.println("Test PASSED! The 'Denis Sobenn' customer full name is displayed on the Invoice");
+		   }else{
+			 System.out.println("Test FAILED! The 'Denis Sobenn' customer full name was not displayed on the Invoice");
+			}
 				 	 	
 		// Close the browser  .//*[@id='top']/div[7]/div/div[9]/table/tbody/tr[1]/td[2]
 		driver.close();
-		
-	
-		// Invoice validation
-		//.//*[@id='top']/div[7]/div/div[7]/div[1]/div[2]
-		// Denis Adventure in invoice
-		//.//*[@id='top']/div[7]/div/div[9]/table/tbody/tr[1]/td[2]/strong[1]
-	
+			
 	}
-
-
 }
